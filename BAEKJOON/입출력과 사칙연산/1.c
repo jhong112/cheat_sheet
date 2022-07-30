d963d5664c81c815ce0c4593c42d2bbf85766749
JaeHong <108808306+jhong112@users.noreply.github.com>
2022-07-30 13:28:56 +0900

    Update 1.c

--- BAEKJOON/입출력과 사칙연산/1.c
+++ BAEKJOON/입출력과 사칙연산/1.c
@@ -4,3 +4,161 @@ int main()
     printf("Hello World!");
     return 0;
 }
+
+
+#include <stdio.h>
+int main()
+{
+    printf("강한친구 대한육군\n");
+    printf("강한친구 대한육군");
+
+    return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+    printf("\\    /\\\n");
+    printf(" )  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+
+    return 0;
+}
+
+
+#include <stdio.h>
+int main()
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|");
+
+    return 0;
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A + B);
+    return 0;
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A-B);
+    return 0;
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d", A*B);
+    return 0;
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    int A, B;
+    double C;
+    scanf("%d %d", &A, &B);
+    C = (double)A / B;
+    printf("%.10f", C);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d\n", A+B);
+    printf("%d\n", A-B);
+    printf("%d\n", A*B);
+    printf("%d\n", A/B);
+    printf("%d", A%B);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    char id[60];
+    scanf("%s", id);
+    printf("%s??!", id);
+}
+
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    printf("%d", a - 543);
+
+}
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d\n%d\n%d\n%d", (a+b)%c,(a%c+b%c)%c,(a*b)%c,((a%c)*(b%c))%c);
+
+}
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+
+int main()
+{
+    int a,b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d\n", b % 10*a);
+    printf("%d\n", b/10 % 10*a);
+    printf("%d\n", b / 100 % 10*a);
+    printf("%d", a * b);
+    return 0;
+}
+
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main()
+{
+    printf("         ,r'\"7\n");
+    printf("r`-_   ,'  ,/\n");
+    printf(" \\. \". L_r'\n");
+    printf("   `~\\/\n");
+    printf("      |\n");
+    printf("      |\n");
+}
