9f00bbc8a3b86c1ba35320e2b5bfd59b0820b141
JaeHong <108808306+jhong112@users.noreply.github.com>
2022-10-31 08:40:51 +0900

    Create 코딩테스트용.c

---
+++ BAEKJOON/문제-단계별로풀어보기/코딩테스트용.c
@@ -0,0 +1,13 @@
+node ptr;
+    node store;
+    store=ptr;
+    
+    ptr.data='a';
+    ptr.next=NULL;
+    
+    ptr=*ptr.next;
+    ptr.data='b';
+    ptr.next=NULL;
+    
+    ptr=store;
+    printf("%c",ptr.data);
